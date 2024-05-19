96421623baa1fd358a542a630541bc51789a6dee
Jacov Plante-Montminy <Jacov_11@hotmail.com>
2024-05-19 12:26:08 +0200

    Ajoute le titre de la page, le paragraphe et ajuste les variables globales.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,14 @@ import ButtonLink from '@/components/ButtonLink/ButtonLink';
 export default function Home() {
   return (
     <main className={styles.main}>
-          <ButtonLink href="#" text="Faites le test sans plus tarder" iconUrl="/rocket-icon.svg" iconAlt="Icône de rocket" />
+      <div className={styles.infoSection}>
+        <h1>Nous vous accompagnons dans votre <span>expansion digitale</span></h1>
+        <p>Le programme Digital Booster d’Expansion est <span>l’atout digital des petites, moyennes et grandes entreprises</span> qui souhaitent développer ou confirmer leur présence en ligne.</p>
+        <ButtonLink href="#" text="Faites le test sans plus tarder" iconUrl="/rocket-icon.svg" iconAlt="Icône de rocket" />
+      </div>
+      <div className={styles.imageSection}>
+
+      </div>
     </main>
   );
 }
