851d2959023c8b578e91fe51f39d2887ac55c88a
Jacov Plante-Montminy <Jacov_11@hotmail.com>
2024-05-19 15:44:31 +0200

    Met l'animation sur la section info plutôt que l'image pour un meilleur focus UX.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ import ButtonLink from '@/components/ButtonLink/ButtonLink';
 import { useGsapAnimation } from '@/hooks/useGsapAnimation';
 
 export default function Home() {
-  const imageSectionRef = useGsapAnimation(
+  const infoSectionRef = useGsapAnimation(
     { opacity: 0, scale: 0.8 },
     { opacity: 1, scale: 1, duration: 2, ease: 'power2.inOut' }
   );
 
   return (
     <main className={styles.main}>
-      <div className={styles.infoSection}>
+      <div className={styles.infoSection} ref={infoSectionRef}>
         <h1>Nous vous accompagnons dans votre <span>expansion digitale</span></h1>
         <p>Le programme Digital Booster d’Expansion est <span>l’atout digital des petites, moyennes et grandes entreprises</span> qui souhaitent développer ou confirmer leur présence en ligne.</p>
         <ButtonLink href="#" text="Faites le test sans plus tarder" iconUrl="/rocket-icon.svg" iconAlt="Icône de rocket" />
       </div>
-      <div className={styles.imageSection} ref={imageSectionRef}>
+      <div className={styles.imageSection}>
       </div>
     </main>
   );
