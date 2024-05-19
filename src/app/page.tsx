774dc4d05430bbc519bc287414e06cc21b160572
Jacov Plante-Montminy <Jacov_11@hotmail.com>
2024-05-19 16:18:00 +0200

    Ajoute une petite animation sur le background-image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,11 @@ export default function Home() {
     { opacity: 1, scale: 1, duration: 2, ease: 'power2.inOut' }
   );
 
+  const imageSectionRef = useGsapAnimation(
+    { opacity: 0 },
+    { opacity: 1, duration: 3, ease: 'power2.inOut' }
+  );
+
   return (
     <main className={styles.main}>
       <div className={styles.infoSection} ref={infoSectionRef}>
@@ -18,7 +23,7 @@ export default function Home() {
         <p>Le programme Digital Booster d’Expansion est <span>l’atout digital des petites, moyennes et grandes entreprises</span> qui souhaitent développer ou confirmer leur présence en ligne.</p>
         <ButtonLink href="#" text="Faites le test sans plus tarder" iconUrl="/rocket-icon.svg" iconAlt="Icône de rocket" />
       </div>
-      <div className={styles.imageSection}>
+      <div className={styles.imageSection} ref={imageSectionRef}>
       </div>
     </main>
   );
